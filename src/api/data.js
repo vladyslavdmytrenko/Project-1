c980c016a20304b9d3bf0afdbc4a8d58cf922db1
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-11-04 10:41:22 +0200

    Task 5: add .env, create server json, fix api file

--- src/api/data.js
+++ src/api/data.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: 'http://localhost:3000/',
+const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+const PORT = process.env.REACT_APP_SERVER_PORT;
+
+const instance = axios.create({
+  baseURL: `${BASE_URL}${PORT}/`,
 });
+
+export default instance;
